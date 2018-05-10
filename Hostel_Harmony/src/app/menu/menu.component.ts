e330509e86665f6da45a3ea89334f7dcd4545b22
elchanan <eli032.eb@gmail.com>
2018-05-10 22:34:35 +0300

    edit menu navbar

--- Hostel_Harmony/src/app/menu/menu.component.ts
+++ Hostel_Harmony/src/app/menu/menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {NameSelectService} from '../service/New folder/name-select.service';
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -7,10 +9,11 @@ import { Router } from '@angular/router';
 })
 export class MenuComponent implements OnInit {
 
-  constructor(private router: Router) { }
-
+  constructor(private router: Router,private data:NameSelectService) { }
+  message:string;
   current = new Date()
-
+  staff:string[]=['עמית','ויסאם','אלחנן','אלון','בן'];
+  resident:string[]=['דייר 1','דייר 2','דייר 3','דייר 4','דייר 5'];
   navigateTo(value) {
     if (value) {
       this.router.navigate([value]);
@@ -18,6 +21,10 @@ export class MenuComponent implements OnInit {
     return false;
   }
   ngOnInit() {
+    this.data.cm.subscribe(message =>this.message=message);
+  }
+  sendVal(selval:string){
+    this.data.changeMessage(selval);
   }
 
 }
