dd8e62d7bc77837680aa747abd5341b781bf27fe
amitJas <katanamit7@gmail.com>
2018-05-13 18:08:15 +0300

    calendar-commit 2

--- Hostel_Harmony/src/app/calendar/calendar.component.ts
+++ Hostel_Harmony/src/app/calendar/calendar.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
 //import { CalendarEvent } from 'calendar-utils';
-import { CalendarEvent } from 'angular-calendar';
+import { CalendarEvent, CalendarEventTitleComponent } from 'angular-calendar';
 import {  ChangeDetectionStrategy } from '@angular/core';
 import {  CalendarMonthViewDay } from 'angular-calendar';
 import { isSameMonth, isSameDay } from 'ngx-bootstrap/chronos/utils/date-getters';
-
-
+import { CustomEventTitleFormatter } from '../provider/custom-event-title-formatter.provider';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./calendar.component.css'],
+  providers: [
+    {
+      provide: CalendarEventTitleComponent,
+      useClass: CustomEventTitleFormatter
+    }
+    
+  ]
 })
 export class CalendarComponent implements OnInit {
   ngOnInit() {
   }
-  
- date = new Date();
+  view: string ='week'
+  viewDate: Date = new Date();
 
   events: CalendarEvent[] = [
     {
@@ -25,23 +32,6 @@ export class CalendarComponent implements OnInit {
     }
   ];
 
-  activeDayIsOpen: boolean;
-
-  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void 
-  {
-    if (isSameMonth(date, this.date)) {
-      if (
-        (isSameDay(this.date, date) && this.activeDayIsOpen === true) ||
-        events.length === 0
-      ) 
-      {
-        this.activeDayIsOpen = false;
-      } else {
-        this.activeDayIsOpen = true;
-        this.date = date;
-      }
-    }
   
   
-  }
 }
