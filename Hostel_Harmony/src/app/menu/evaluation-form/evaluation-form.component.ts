195991d563ea6af8b9082d2ac470a672d1d5a11d
elchanan <eli032.eb@gmail.com>
2018-05-22 02:25:36 +0300

    extra components for menu page

--- Hostel_Harmony/src/app/menu/evaluation-form/evaluation-form.component.ts
+++ Hostel_Harmony/src/app/menu/evaluation-form/evaluation-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-evaluation-form',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./evaluation-form.component.css']
 })
 export class EvaluationFormComponent implements OnInit {
-
+  @Input()//for getting name wanted
+  name:string;
   constructor() { }
 
   ngOnInit() {
