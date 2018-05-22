ae57f6946873362207da0306789f7fa704172274
elchanan <eli032.eb@gmail.com>
2018-05-22 06:37:59 +0300

    add in menu option to fill evaluation form

--- Hostel_Harmony/src/app/menu/evaluation-form/evaluation-form.component.ts
+++ Hostel_Harmony/src/app/menu/evaluation-form/evaluation-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit,Input } from '@angular/core';
+import { EvalForm} from '../../models/eval-form.model'
+import {NgModel,FormGroup,FormArray,FormBuilder ,FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-evaluation-form',
@@ -8,9 +10,47 @@ import { Component, OnInit,Input } from '@angular/core';
 export class EvaluationFormComponent implements OnInit {
   @Input()//for getting name wanted
   name:string;
-  constructor() { }
+  myForm: FormGroup;
+  private model: EvalForm ;
+  private submitted: boolean;
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
 
+  constructor(private fb:FormBuilder ) {
+    this.submitted = false;
+    this.model = new EvalForm(
+      null, 
+      null,
+      [this.buildSemiGoal(null).value]
+    );
+  }
   ngOnInit() {
+    this.myForm = this.fb.group({
+      semiGoal: this.fb.array(
+        [this.buildSemiGoal('')])
+    })
+  }
+  public buildSemiGoal(val: string) {
+    return new FormGroup({
+      goal:new FormControl(val),
+      acts:this.fb.array(
+        [this.buildActs(val)]
+      ),
+      how:new FormControl(val),
+
+    })
+  }
+  public buildActs(val: string) {
+    return new FormGroup({
+        action:new FormControl(val),
+        caretaker: new FormControl(val),
+      })
+    
+  }
+  subGoal(obj:object) {
+    this.submitted = true;/*do something*/
+    alert(this.submitted);
+    console.log(obj);
   }
 
 }
--- Hostel_Harmony/src/app/models/eval-form.model.ts
+++ Hostel_Harmony/src/app/models/eval-form.model.ts
@@ -1,7 +1,15 @@
 export class EvalForm{
     constructor(
-        public value: string,
-    
+        public name: string,
+        public mainGoal: string,
+        public semiGoal:{
+            goal:String,
+            acts:{
+                action:string,
+                caretaker:string
+            }[],
+            how:string
+        }[] 
     ){
 
     }
