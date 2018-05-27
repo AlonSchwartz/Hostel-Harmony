b54495312c4dc33187b4e6a2bd005eaa555ae7ac
elchanan <eli032.eb@gmail.com>
2018-05-27 14:18:28 +0300

    style changes, view resident/staff more to be done

--- Hostel_Harmony/src/app/menu/view/view.component.ts
+++ Hostel_Harmony/src/app/menu/view/view.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit , Input } from '@angular/core';
-
+import {staff} from '../../models/staff.model'
+import {resident} from '../../models/resident.model'
+import { NgModel,FormGroup } from '@angular/forms'
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
@@ -8,7 +10,14 @@ import { Component, OnInit , Input } from '@angular/core';
 export class ViewComponent implements OnInit {
   @Input()//for getting name wanted
   name:string;
-  constructor() { }
+  submitted:boolean
+  vstaff:staff
+  vresident:resident
+  constructor() {
+    this.submitted=false
+    this.vstaff=new staff('moshe','cohen',0,true,'26',3,'pert@il','king');
+    this.vresident=new resident('moshe','aviv',0,true,'24',-1,'ben','ben','2018',false,[{rel:'dd',name:'mm',phone:0},{rel:'dn',name:'mmgg',phone:0}],{info:'',phone:0,location:''},{psych:'',gp:''});
+   }
 
   ngOnInit() {
   }
